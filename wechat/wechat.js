354d86fc5b18b9a585f5c4b7e42a884fbf1a39b8
薛聪聪 <xcc1997@gmail.com>
2017-03-01 17:37:56 +0800

    天气api更换为和风天气

--- wechat/wechat.js
+++ wechat/wechat.js
@@ -28,14 +28,19 @@ exports.response = function (req, res) {
                 var weather = text.indexOf('天气');
                 if(weather != -1 ){
                     var location = text.replace('天气','');
-                    https.get('https://api.thinkpage.cn/v3/weather/now.json?key=90cchlqs5ekrtutj&location='+ urlencode(location) +'&language=zh-Hans&unit=c', function(ress){
+                    https.get('https://free-api.heweather.com/v5/weather?city='+ urlencode(location) +'&key=498ffe4ba487474b8f4b00ed9cab7bd1', function(ress){
                         ress.on('data', function (json) {
                             json = JSON.parse(json);
                             if (json.status_code) {
                                 send.text('天气获取失败，可能是输入了错误的地名或格式不正确。')
                             } else {
-                                send.text(location + '：\n' +
-                                    '当前天气：' + json.results[0].now.text + ' 气温：' + json.results[0].now.temperature + '℃');
+                                send.text(location + '天气（' + json.HeWeather5["0"].basic.update.loc.slice(11) +'数据）\n'
+                                    + '现在：\n'
+                                    + json.HeWeather5["0"].now.cond.txt + '，' + json.HeWeather5["0"].now.tmp + '℃，' + '风力' + json.HeWeather5["0"].now.wind.sc + '级，空气质量：' + json.HeWeather5["0"].aqi.city.qlty + '。\n'
+                                    + '最低气温：' + json.HeWeather5["0"].daily_forecast[0].tmp.min + '℃，最高气温' + json.HeWeather5["0"].daily_forecast[0].tmp.max + '℃。\n'
+                                    + '明日：\n'
+                                    + json.HeWeather5["0"].daily_forecast[1].cond.txt_d + '转' + json.HeWeather5["0"].daily_forecast[1].cond.txt_n + '，气温：' + json.HeWeather5["0"].daily_forecast[1].tmp.min + '℃~' + json.HeWeather5["0"].daily_forecast[1].tmp.max + '℃。'
+                                );
                             }
                         });
                     }).on('error', function (e) {
