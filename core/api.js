1d4d28a4520fb186c507368b7aa88a63b880e835
薛聪聪 <xcc1997@gmail.com>
2017-03-01 16:54:30 +0800

    增加简单天气查询

--- core/api.js
+++ core/api.js
@@ -1,13 +1,14 @@
 var https = require('https');
 
 //api访问
-exports.get = function (url, callbacke, callback) {
+exports.get = function (url, callback) {
     https.get(url, function (ress) {
         ress.on('data', function (data) {
+            console.log('获取到data:'+ data);
             callback(data);
         })
     }).on('error', function (e) {
-        callbacke(e);
+        callback(e);
         });
 };
 
--- wechat/wechat.js
+++ wechat/wechat.js
@@ -1,7 +1,9 @@
 var wxsha1 = require('../core/wxsha1');
 var xml2js = require('xml2js');
 var handle = require('./handle');
-
+var api = require('../core/api');
+var https = require('https');
+var urlencode = require('urlencode');
 
 exports.check = function (req, res) {
     var signature = req.query.signature;
@@ -18,12 +20,28 @@ exports.check = function (req, res) {
 };
 exports.response = function (req, res) {
     var xml = req.body;
-    xml2js.parseString(xml, {explicitArray: false}, function (err,data) {
+    xml2js.parseString(xml, {explicitArray: false}, function (err, data) {
         if(err){
             console.error(err);
         } else {
-            handle.do(res, data, function (text,send) {
-                send.text(text);
+            handle.do(res, data, function (text, send) {
+                var weather = text.indexOf('天气');
+                if(weather != -1 ){
+                    var location = text.replace('天气','');
+                    https.get('https://api.thinkpage.cn/v3/weather/now.json?key=90cchlqs5ekrtutj&location='+ urlencode(location) +'&language=zh-Hans&unit=c', function(ress){
+                        ress.on('data', function (json) {
+                            json = JSON.parse(json);
+                            if (json.status_code) {
+                                send.text('天气获取失败，可能是输入了错误的地名或格式不正确。')
+                            } else {
+                                send.text(location + '：\n' +
+                                    '当前天气：' + json.results[0].now.text + ' 气温：' + json.results[0].now.temperature + '℃');
+                            }
+                        });
+                    }).on('error', function (e) {
+                        console.log(e);
+                    });
+                }
             });
         }
     });
